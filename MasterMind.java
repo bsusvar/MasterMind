67fb672be4c8143be1150247af373c1d9faf59a2
Beatriz <bsusinovargas@gmail.com>
2024-11-25 20:19:04 +0100

    Añadir métodos al programa para clarificar el código

--- MasterMind.java
+++ MasterMind.java
@@ -8,36 +8,21 @@ public class MasterMind {
 
 	public static void main(String[] args) {
 
-		// Creación de un String con los números
-		String lista = ("123456789");
-		char[] codigo = lista.toCharArray();
-
-		// Mezcla aleatoria de los números (caracteres) del array intercambiando sus
-		// posiciones
-		int i = 0;
-		int j = 0;
-		char temp = 0;
-		for (i = 0; i < codigo.length - 1; i++) {
-			j = (int) (Math.random() * codigo.length);
-			temp = codigo[i];
-			codigo[i] = codigo[j];
-			codigo[j] = temp;
-		}
-
-		// Selección de los 4 dígitos del código secreto
-		char[] codigoSecreto = Arrays.copyOfRange(codigo, 0, 4);
-		System.out.println(codigoSecreto);
-
-		// Introducción al juego
-		Scanner input = new Scanner(System.in);
-		System.out.println(
-				"¡Bienvenido/a! ¿Te atreves a desafiar a la mente maestra? Intenta adivinar los 4 dígitos secretos: ");
 		String respuestaUsuario = "s";
-
 		// Bucle que realiza la partida siempre que el usuario quiera continuar
 		// escribiendo 's' (sí)
+
+		System.out.println("¡Bienvenido/a! ¿Te atreves a desafiar a la mente maestra?");
+
 		do {
+
+			char[] codigoSecreto = mezclarNumeros();
+
+			// Introducción al juego
+			Scanner input = new Scanner(System.in);
+
 			// Comprobación de la respuesta del usuario
+			System.out.println("Intenta adivinar los 4 dígitos secretos: ");
 			int contadorIntentos = 10;
 			char[] codigoUsuario;
 			do {
@@ -57,29 +42,44 @@ public class MasterMind {
 						+ contarExactos(codigoUsuarioInt, codigoSecretoInt) + "\n" + "Dígitos acertados cercanos: "
 						+ contarCercanos(codigoUsuarioInt, codigoSecretoInt));
 
-				if (!usuarioGanador(codigoUsuario, codigoSecreto)) {
+				if (!usuarioGanador(codigoUsuario, codigoSecreto) && usuarioSinIntentos(contadorIntentos)) {
 					System.out.println("Te quedan " + contadorIntentos + " intentos: ");
 				}
 
 			} while (!usuarioGanador(codigoUsuario, codigoSecreto) && usuarioSinIntentos(contadorIntentos));
 
 			// Fin de la partida (usuario ganador)
-			if (usuarioGanador(codigoUsuario, codigoSecreto) == true) {
-				System.out.println("¡Has ganado! ¿Quieres jugar otra partida? Escribe 's' (sí) o 'n' (no): ");
-				respuestaUsuario = input.nextLine();
-				finPartida(respuestaUsuario);
-			}
-
-			// Fin de la partida (usuario sin intentos restantes)
-			else if (usuarioSinIntentos(contadorIntentos) == false) {
-				System.out.println(
-						"Te has quedado sin intentos. ¿Quieres jugar otra partida? Escribe 's' (sí) o 'n' (no): ");
-				respuestaUsuario = input.nextLine();
-				finPartida(respuestaUsuario);
-			}
+			respuestaUsuario = generarFinPartida(codigoUsuario, codigoSecreto, contadorIntentos);
 		}
 
 		while (respuestaUsuario.equals("s"));
+	}
+
+	// Método para mezclar los números
+
+	private static char[] mezclarNumeros() {
+
+		// Creación de un String con los números
+		String lista = ("123456789");
+		char[] codigo = lista.toCharArray();
+
+		// Mezcla aleatoria de los números (caracteres) del array intercambiando sus
+		// posiciones
+		int i = 0;
+		int j = 0;
+		char temp = 0;
+		for (i = 0; i < codigo.length - 1; i++) {
+			j = (int) (Math.random() * codigo.length);
+			temp = codigo[i];
+			codigo[i] = codigo[j];
+			codigo[j] = temp;
+		}
+
+		// Selección de los 4 dígitos del código secreto
+		char[] codigoSecreto = Arrays.copyOfRange(codigo, 0, 4);
+		// System.out.println(codigoSecreto);
+
+		return codigoSecreto;
 	}
 
 	// Método para convertir a arrays de enteros
@@ -138,11 +138,24 @@ public class MasterMind {
 		return contadorAciertos;
 	}
 
-	// Método que detiene la partida si el usuario no quiere continuar jugando
-	private static String finPartida(String respuestaUsuario) {
+	// Método que genera el fin de partida si el usuario gana o si se ha quedado sin
+	// intentos
+	private static String generarFinPartida(char[] codigoUsuario, char[] codigoSecreto, int contadorIntentos) {
+		Scanner input = new Scanner(System.in);
+		// Fin de la partida (usuario ganador)
+		if (usuarioGanador(codigoUsuario, codigoSecreto) == true) {
+			System.out.println("¡Has ganado! ");
+		}
+
+		// Fin de la partida (usuario sin intentos restantes)
+		else if (usuarioSinIntentos(contadorIntentos) == false) {
+			System.out.println("Te has quedado sin intentos. ");
+		}
+		System.out.println("¿Quieres jugar otra partida? Escribe 's' (sí) o 'n' (no): ");
+		String respuestaUsuario = input.nextLine();
 		respuestaUsuario.toLowerCase();
 		if (respuestaUsuario.equals("n")) {
-			System.out.println("De acuerdo, ¡hasta la próxima partida! :-)");
+			System.out.println("Vale, ¡hasta la próxima partida! :-)");
 		}
 		return respuestaUsuario;
 	}
